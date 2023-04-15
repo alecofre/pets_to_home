569ea5638031d07b954c06aa1fa9c678bc18e0f2
Ale Cofré <ale.cofre.perez@gmail.com>
2023-04-15 13:33:08 -0400

    Se crea model Pet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+city = City.create(name: "Viña del Mar")
+city = City.create(name: "Valparaiso")
+city = City.create(name: "Quilpue")
+city = City.create(name: "Villa Alemana")
+city = City.create(name: "Limache")
+city = City.create(name: "Olmue")
+city = City.create(name: "ConCon")
+
+pet = Pet.create(name: "Perro")
+pet = Pet.create(name: "Gato")
+pet = Pet.create(name: "Hamster")
+pet = Pet.create(name: "Conejo")
+pet = Pet.create(name: "Pez")
+pet = Pet.create(name: "Pajaro")
+pet = Pet.create(name: "Tortuga")
