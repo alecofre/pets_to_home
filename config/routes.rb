8b353ec9016f141662297b1986c689319049440a
Ale Cofré <ale.cofre.perez@gmail.com>
2023-04-15 13:29:39 -0400

    Configuracion de User (con devise)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :kindergartens
+  resources :pets
+  resources :cities
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "cities#index"
 end
